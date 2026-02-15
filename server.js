9c9b26d528fae0ed531fe8bfe2244e287c846154
didarBro <didar131299@gmail.com>
2026-02-15 22:35:53 +0600

    fix: database connection

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`Base URL: http://localhost:${PORT}`);
 });
 
 // Handle unhandled promise rejections
@@ -16,4 +17,3 @@ process.on("unhandledRejection", (err) => {
   console.error(`Error: ${err.message}`);
   server.close(() => process.exit(1));
 });
-
--- src/models/User.model.js
+++ src/models/User.model.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Password is required"],
       minlength: [6, "Password must be at least 6 characters"],
-      select: false, // Don't return password by default
+      select: false, // Do not return password by default
     },
   },
   {
@@ -33,29 +33,27 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-// Hash password before saving
-userSchema.pre("save", async function (next) {
-  // Only hash the password if it has been modified (or is new)
-  if (!this.isModified("password")) {
-    return next();
-  }
+//
+// ✅ Hash password before saving (Modern Mongoose way)
+//
+userSchema.pre("save", async function () {
+  // Only hash password if it is new or modified
+  if (!this.isModified("password")) return;
 
-  try {
-    // Hash password with cost of 12
-    const salt = await bcrypt.genSalt(12);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
-// Method to compare password
+//
+// ✅ Compare password method
+//
 userSchema.methods.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
-// Remove password from JSON output
+//
+// ✅ Remove password from JSON responses
+//
 userSchema.methods.toJSON = function () {
   const userObject = this.toObject();
   delete userObject.password;
@@ -63,4 +61,3 @@ userSchema.methods.toJSON = function () {
 };
 
 module.exports = mongoose.model("User", userSchema);
-
